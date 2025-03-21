1b8de50ed4928819d39e0ffe45667bb48ba93bc8
anhhgi <vanhyoyo@gmail.com>
2025-03-21 16:19:26 +0700

    add element plust

---
+++ components/ui/vc-button.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-button
+    :class="['vc-btn', button_class]"
+    :type="type"
+    :size="size"
+    :loading="loading"
+    @click="onClick"
+  >
+    <slot name="icon" />
+    <slot />
+  </el-button>
+</template>
+
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: "primary",
+  },
+  size: {
+    type: String,
+    default: "default",
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  button_class: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(["click"])
+
+const onClick = (event) => {
+  emit("click", event)
+}
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/scss/components/vc-button.scss" as *;
+</style>
--- pages/client/home/index.vue
+++ pages/client/home/index.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Client Home</h1>
-</template>
+    <div class="home">
+      <h1>Chào mừng đến với Nuxt App!</h1>
+      <vc-button type="primary" @click="handleClick">Nút Element Plus</vc-button>
+    </div>
+  </template>
+  
+  <script setup>
+  const handleClick = () => {
+    alert('Bạn đã click nút Element Plus!')
+  }
+  </script>
+  
