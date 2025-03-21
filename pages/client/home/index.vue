a85c236bd28e23498e2281f35cf8eccffd555bd5
anhhgi <vanhyoyo@gmail.com>
2025-03-21 16:23:19 +0700

    add prettierrc

--- pages/client/home/index.vue
+++ pages/client/home/index.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class="home">
-      <h1>Chào mừng đến với Nuxt App!</h1>
-      <vc-button type="primary" @click="handleClick">Nút Element Plus</vc-button>
-    </div>
-  </template>
-  
-  <script setup>
-  const handleClick = () => {
-    alert('Bạn đã click nút Element Plus!')
-  }
-  </script>
-  
+  <div class="home">
+    <h1>Chào mừng đến với Nuxt App!</h1>
+    <vc-button type="primary" @click="handleClick">Nút Element Plus</vc-button>
+  </div>
+</template>
+
+<script setup>
+const handleClick = () => {
+  alert('Bạn đã click nút Element Plus!')
+}
+</script>
